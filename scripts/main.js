f28f17605377b3ec2b0be5e02468640915725e9c
Caoimhe <caoimhegmalone@gmail.com>
2018-12-13 08:56:49 +0000

    Delete main.js

--- scripts/main.js
+++
@@ -1,7 +0,0 @@
-// For scroll to the top animation 
-$('#scrolltotop').click(function() {      // When arrow is clicked
-    $('body,html').animate({
-        scrollTop : 0                       // Scroll to top of body
-    }, 500);
-});
-
