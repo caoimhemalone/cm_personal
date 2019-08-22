efe4272bdade18edf256305b57d0819a33b6511c
caoimhemalone <caoimhegmalone@gmail.com>
2019-08-22 20:45:20 +0100

    updates

---
+++ v.0.5/blog/blog-1.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+     <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>key-vah | Caoimhe Malone</title>
+    <meta name="description" content="keyvah.com | Caoimhe Malone">
+    <meta name="author" content="Caoimhe Malone" />
+
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
+    <link rel="stylesheet" href="../styles/plugins.min.css" type="text/css" media="screen" />
+    <!-- <link rel="stylesheet" href="styles/main.css" type="text/css" media="screen" /> -->
+    <link href="../Bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"   integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="   crossorigin="anonymous"></script>
+
+    <style type="text/css"><?php include("../styles/main.css");?></style>
+</head>
+
+
+<body>
+    <section class="blog-post-content">
+        <div class="inline-menu-container showx">
+            <span class="status contact menu-item">I am available for freelance</span>
+            <ul class="inline-menu">
+                <li class="about menu-item"><a href="/">What I've Done</a></li>
+                <li class="portfolio menu-item"><a href="/">Portfolio</a></li>
+                <li class="blog menu-item"><a href="/">Blog</a></li>
+                <li class="contact menu-item"><a href="/">Contact</a></li>
+            </ul>
+        </div>
+
+        <div id="blog-header" style="background: url(../desk.jpeg); background-size: cover;">
+            <div class="overlay"></div>
+            <div class="single-post-title">
+                <h1>Why I don’t think the 5am club is the only way to be productive</h1>
+                <p class="post-info">
+                    <span class="post-author">Caoimhe Malone</span>
+                    <span class="slash"></span>
+                    <span class="post-date">Jan 31, 2019</span>
+                    <span class="slash"></span>
+                    <span class="post-catetory">Productivity</span>
+                </p>
+            </div>
+        </div>
+        <div class="block-content">
+            <div class="row">
+                <div class="col-md-12">
+                    <div id="single-post">
+                        <div class="post">
+                            <div class="post-body">
+                                <p>This started off as a journal entry but I decided that it was actually a good topic to write about.</p>
+                                <!-- <blockquote>
+                                    A blockquote example goes here.Sed convallis consectetur sem, id hendrerit massa posuere nec.
+                                </blockquote> -->
+                                <p>I’ve been thinking a lot about the 5am club. I’ve heard about it on many podcasts and have read numerous medium articles on the subject. I would 100% join the 5am club instead of staying up until 11:30 doing my side job of web dev or editing a video (which I rarely have time to do anymore). But right now in my life it’s just not ideal to get up at 5am.</p>
+                                <p>Yeah it would be great by the time I get into work at 8:30 I can have my gym workout done, a bit of web development done, my breakfast eaten nice and early to get my metabolism going and my 10km cycle to work done. I could have a really productive day before 9am.</p>
+                                <p>But there’s just one problem.</p>
+                                <p>It would take a huge toll on my relationship with my boyfriend. Our weekdays are so busy that we only get to talk to each other via text in the evening (and then we see each other on the weekend). And even at that the conversation is spread out because we aren’t looking at our phones the whole evening. We don’t live together so it makes catching up on eachothers day a little bit harder. So if I woke up at 5am I would have to go to sleep at 9pm to get my 8 hours sleep ( I need my 8 hours or I can’t function). And I just can’t do that right now.</p>
+                                <p>But my current routine does work for me.</p>
+                                <h2>My Daily (weekday) Routine</h2>
+                                <p>I wake up at 7:09 am to the sound of Alexa telling me the weather for the day (essential when living in Ireland and cycling to work). Then my alarm goes off at 7:10am indicating that it’s time to get out of bed. I eat a banana, that I bring to my room the night before, and get dressed into my gym/cycling gear. I brush my teeth then go downstairs and grab my lunch from the fridge. I put on everything I need to cycle (jacket, gloves, helmet, turn lights on) then leave the house around 7:30–7:45. I cycle 10km (40minutes) from Santry, Dublin to Leeson Street, Dublin. I get into work (a web development company) for 8:10–8:20, have a shower and get into my work clothes (the typical Developer attire of jeans and a hoodie) then I am at my desk by 8:30/40. Around 9am I will have my porridge (porridge oats, milk, peanut butter, chia seeds, and protein powder) It’s the best porridge ever. Then I work until 5pm. I’m back on the bike at 5:15pm, cycle 8.4km to the gym, do an hours workout, cycle ~2km home, get home around 7:15, have my dinner, shower or have a bath to relax my muscles while I read a book, then some days I sit down at my computer and work on my side hustle of Web Developement for a few local businesses. I’ll do maybe 2/2.5 hours of this then go to bed.</p>
+                                <p>A day well spent and I didn’t need to get up at 5am to do it.</p>
+                                <p>So yeah getting up at 5am means that you are productive early in the day but it doesn’t mean that getting up any later will make you less productive.</p>
+                                <p>Maybe in the future when me and my boyfriend are living together we can both join the 5am club and make it work. But for now my routine is pretty solid.</p>
+
+                            </div>
+                            <div class="author-box row">
+                                <div class="col-sm-2">
+                                    <img src="../me.jpeg" class="rounded-circle" alt="..." />
+                                </div>
+                                <div class="col-sm-10">
+                                    <h3>Caoimhe Malone<span> | Author</span></h3>
+                                    <p>Frontend Web Developer, Computer Science Graduate, Aspiring Photographer & Filmmaker, Geek, Apple Fangirl and yes I still play Pokemon GO</p>
+                                </div>
+                            </div>
+                            <ul id="blog-social">
+                                <li>Follow Me For More Content : </li>
+                                <li><a href="https://www.instagram.com/caoimhemalone" target="_blank"><i class="ion-social-instagram"></i></a> </li>
+                                <li><a href="https://www.twitter.com/caoimhemalone95" target="_blank"><i class="ion-social-twitter"></i></a> </li>
+                                <li><a href="https://medium.com/@caoimhegmalone" target="_blank"><i class="ion-ios-book"></i></a> </li>
+                                <li><a href="https://www.youtube.com/keyvah" target="_blank"><i class="ion-social-youtube"></i></a> </li>
+                            </ul>
+                            <!-- <div class="project-nav text-center">
+                                <span class="float-left">
+                                    <a class="open-project" href="project-5.html">&leftarrow; Previous Project</a>
+                                </span>
+                                                <span class="">
+                                    <a id="close-project" href="../layout.php"><i class="ion-grid"></i></a>
+                                </span>
+                                                <span class="float-right">
+                                    <a class="open-project" href="project-1.html">Next Project &rightarrow; </a>
+                                </span>
+                            </div> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <script><?php include("../scripts/min/main.js");?></script>
+    <script src="../scripts/plugins.min.js"></script>
+    <script src="../Bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
