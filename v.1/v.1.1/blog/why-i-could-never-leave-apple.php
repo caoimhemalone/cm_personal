1e9a4be22290c8c1bb04d9c2b02a2b28461333a1
Caoimhe Malone <caoimhemalone@Caoimhes-MacBook-Air.local>
2019-09-26 18:14:05 +0100

    new blog post

---
+++ v.1/v.1.1/blog/why-i-could-never-leave-apple.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+          <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>keyvah | Caoimhe Malone</title>
+    <meta name="description" content="How Apple has engulfed me in its ecosystem | key-vah.com | Caoimhe Malone">
+    <meta name="author" content="Caoimhe Malone" />
+    
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    
+    <link rel="canonical" href="https://www.key-vah.com/blog/why-i-could-never-leave-apple" />
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-139553932-2"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'UA-139553932-2');
+    </script>
+
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
+    <link rel="stylesheet" href="../styles/plugins.min.css" type="text/css" media="screen" />
+    <!-- <link rel="stylesheet" href="styles/main.css" type="text/css" media="screen" /> -->
+    <link href="../Bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"   integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="   crossorigin="anonymous"></script>
+
+    <style type="text/css"><?php include("../styles/main.css");?></style>
+</head>
+
+
+<body>
+    
+    <section class="blog-post-content">
+        <?php include("../includes/other-pages-menu.php");?>
+
+        <div id="blog-header" style="background: url(../apple-products.jpeg); background-size: cover;">
+            <div class="overlay"></div>
+            <div class="single-post-title">
+                <h1>Why I could never leave Apple</h1>
+                <p class="post-info">
+                    <span class="post-author">Caoimhe Malone</span>
+                    <span class="slash"></span>
+                    <span class="post-date">Sep 26, 2019</span>
+                    <span class="slash"></span>
+                    <span class="post-catetory">Tech</span>
+                </p>
+            </div>
+        </div>
+        <div class="block-content">
+            <div class="row">
+                <div class="col-md-12">
+                    <div id="single-post">
+                           <span style="font-size: 0.7em;">Photo by  <a href="#" onclick='window.open("https://unsplash.com/@rev3n");return false;'>Michał Kubalczyk</a> on Unsplash</span>
+                        <div class="post">
+                            <div class="post-body">
+                                <p>With Apple's release of the iPhone 11 and 11 Pro this month I thought I’d dive into why I don’t see myself leaving Apple. </p>
+
+                                <p>At 24 I am on iPhone number 4 (iPhone  5, 6, 7, XS), that’s not exactly a high number but every second year since I was 17 I bought a new iPhone and renewed my 2-year contract with Three without hesitation. I have not always paid for my bill so that might have a factor in being able to get a new iPhone every second year.</p>
+                                <p>When I was 17 myself and my sister made a presentation on Keynote, sat my parents down and made points as to why they should get us an iPhone (Looking back now it makes us sound so spoiled). My parents were reluctant but we kept persisting we even changed my Mam's lock screen of her iPhone 4 to the new iPhone 5. After we hounded them for months my mam decided that we could get them. Because she was on a work contract she wasn’t paying her phone bill herself so she decided that she would pay for ours instead. She also liked the idea of us never running out of credit in an emergency because it was bill pay we could always call her. I think she paid the few hundred for each phone too. After that we would pay the €300-500 whatever it was for the phone and my mam would pay the bill. Until I got my iPhone XS. I was just graduated from college, in a full-time job and decided to buy myself a Christmas present. I paid around €600 for the phone and for the first time my phone bill myself. And ever since I have never even considered any other brand of phone.</p>
+                                
+                                <p>So why no matter the cost do I keep going back to Apple again and again?</p>
+                                <p>I have the first generation iPad, 2nd gen Apple TV, I had gen 0 Apple Watch and last summer upgraded to gen 4, Airpods 1, I have a 2015 iMac, a dinosaur 2008 iMac (now used mainly as a photos slideshow), I have the 2012 MacBook Pro (extremely slow and only used for Netflix) and recently thanks to my way too generous boyfriend I have the 2018 MacBook Air. </p>
+                                <p>This may make me sound like I’m made of money but these were all accumulated over the past 10 years. I like to save up my money to buy expensive technology and I’m an Apple Nerd so I happily give them all of my hard-earned money. </p>
+                                <p>But why? Why do I give this trillion-dollar company all of my money? </p>
+                                <p>Because I believe in them. I believed in Steve Jobs. He wasn’t perfect by any means but there was a method to his madness. Without him being anal about designs he wouldn’t have revolutionised certain products. Like the swivel neck iMac or the first iPad or how the floors and windows looked in his Apple Stores. I think my belief in what he built and what Apple is today is why I am so blindly devoted to the company. </p>
+                                <p>If I hadn’t of been so inspired by Steve Jobs when I was 15 then I probably wouldn’t have gone on to do Computer Science in college and wouldn’t have my career today in Web Development (can you call it a career when you’re only 1 year graduated from college?). If I didn’t mess around in iMovie with my families 2008 iMac I would never have gotten into film editing and photography later on as a hobby. </p>
+                                <p>Apple has made me the person that I am. No, seriously, it has. That is why I think I will never veer away from their products. Every time an Android user complains about Apples walled garden I just brush it off and continue to give my support to my favourite company. Every time someone makes a joke about the headphone jack I tell them of how AirPods are the best product that I’ve ever bought and that I’m about to buy my second pair. Every time someone gives out about the price or how Apple is late to the party with some features I know that’s it’s because perfection takes time and that I’d rather pay €1000-2000 for the aluminium to have a long-lasting laptop (my 2012 MacBook Pro is still kicking in her old age). Every time I watch a YouTuber buy a new Windows laptop because Apple took away the ports I know that in a few years those same people will be rocking USB C everything. </p>
+                                <p>I believe that they thrive to make the best products that they can without forcing a project out that isn't perfect (cough cough Galaxy Fold) and they give you something that you didn't even realise that you wanted until you had it. The Floppy Disk reader under the screen. A device that allowed you to listen to 1000s of songs and fits in your pocket. A phone with no physical keyboard. A tablet which fit perfectly between your two hands. A laptop that could fit inside an a4 envelope. A watch with a computer inside. A phone with no headphone jack. Wireless earphones. A phone with 2 cameras on the back. A phone with 3 cameras on the back. And that's just the hardware. Apple has given us so many features over the years in their Software that we just wouldn't have used or known that we wanted to use them unless Apple had taken the initiative to give us those features. They know their users so well and in return have so much loyalty from their users.</p>
+                                <p>So yes I am probably way too loyal to Apple but I wouldn’t have it any other way. I join countless other people who count down the days to WWDC, who sit outside Apple Stores for days waiting for the new iPhone, who save up all of their extra money to buy the newest Apple Products because we just need to have them! We're Apple fangirls/boys devoted to the company that has touched us all in different ways. The reason why I won't leave them is simple; everything just works.</p>
+                                
+
+                            </div>
+                            <div class="author-box row">
+                                <div class="col-sm-2">
+                                    <img src="../me.jpg" class="rounded-circle" alt="..." />
+                                </div>
+                                <div class="col-sm-10">
+                                    <h3>Caoimhe Malone<span> | Author</span></h3>
+                                    <p>Frontend Web Developer, Computer Science Graduate, Aspiring Photographer & Filmmaker, Geek, Apple Fangirl and yes I still play Pokemon GO</p>
+                                </div>
+                            </div>
+                            <ul id="blog-social">
+                                <li>Follow Me For More Content : </li>
+                                <li><a href="https://www.instagram.com/caoimhemalone" target="_blank"><i class="ion-social-instagram"></i></a> </li>
+                                <li><a href="https://www.twitter.com/caoimhemalone95" target="_blank"><i class="ion-social-twitter"></i></a> </li>
+                                <li><a href="https://medium.com/@caoimhegmalone" target="_blank"><i class="ion-ios-book"></i></a> </li>
+                                <li><a href="https://www.youtube.com/keyvah" target="_blank"><i class="ion-social-youtube"></i></a> </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <script><?php include("../scripts/min/main.js");?></script>
+    <script src="../scripts/plugins.min.js"></script>
+    <script src="../Bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
--- v.1/v.1.1/includes/blog.php
+++ v.1/v.1.1/includes/blog.php
@@ -6,6 +6,29 @@
                     <!--LATEST POST-->
                     <div class="post col-md-6">
                         <div class="post-thumbnail">
+                            <a class="open-post" href="/blog/why-i-could-never-leave-apple">
+                                <img class="lazyload" data-src="apple-products.jpeg" alt="airpods">
+                            </a>
+                        </div>
+                        <div class="post-title">
+                            <a class="open-post" href="https://www.key-vah.com/blog/why-i-could-never-leave-apple"><h2>Why I could never leave Apple</h2></a>
+                            <p class="post-info">
+                                <span class="post-author">Caoimhe Malone</span>
+                                <span class="slash"></span>
+                                <span class="post-date">Sep 26, 2019</span>
+                                <span class="slash"></span>
+                                <span class="post-catetory">Tech</span>
+                            </p>
+                        </div>
+                        <div class="post-body">
+                            <p>If I hadn’t of been so inspired by Steve Jobs when I was 15 then I probably wouldn’t have gone on to do Computer Science in college and wouldn’t have my career today in Web Development...</p>
+                            <a class="btn open-post" href="https://www.key-vah.com/blog/why-i-could-never-leave-apple">Read More</a>
+                        </div>
+                    </div>
+                    <!--END OF LATEST POST-->
+                    
+                    <div class="post col-md-6">
+                        <div class="post-thumbnail">
                             <a class="open-post" href="/blog/airpods-covers-review">
                                 <img class="lazyload" data-src="airpods.jpeg" alt="airpods">
                             </a>
@@ -21,11 +44,10 @@
                             </p>
                         </div>
                         <div class="post-body">
-                            <p>I have had my Apple AirPods V1 for 2 years now (oh so hipster of me I know) and they’re the best thing I never expected to love. I will get into my relationship with my AirPods in another post in a few weeks as I’m thinking about getting </p>
+                            <p>I have had my Apple AirPods V1 for 2 years now (oh so hipster of me I know) and they’re the best thing I never expected to love. I will get into my relationship with my AirPods in another post in a few weeks as I’m thinking about getting... </p>
                             <a class="btn open-post" href="https://www.key-vah.com/blog/airpods-covers-review">Read More</a>
                         </div>
                     </div>
-                    <!--END OF LATEST POST-->
                     
                     <div class="post col-md-6">
                         <div class="post-thumbnail">
@@ -44,7 +66,7 @@
                             </p>
                         </div>
                         <div class="post-body">
-                            <p>This started off as a journal entry but I decided that it was actually a good topic to write about.I’ve been thinking a lot about the 5am club. I’ve heard about it on many podcasts and have read numerous medium articles on the subject. I would 100% join the 5am club instead of staying up until</p>
+                            <p>This started off as a journal entry but I decided that it was actually a good topic to write about.I’ve been thinking a lot about the 5am club. I’ve heard about it on many podcasts and have read numerous medium articles on the subject. I would 100% join the 5am club instead of staying up until...</p>
                             <a class="btn open-post" href="https://www.key-vah.com/blog/the-5am-club">Read More</a>
                         </div>
                     </div>
