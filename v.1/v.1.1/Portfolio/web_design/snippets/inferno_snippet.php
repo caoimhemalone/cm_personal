f7ed2248b027847499ea446d3d0e0ceabbf61024
caoimhemalone <caoimhegmalone@gmail.com>
2020-04-30 22:11:35 +0100

    inferno website added to portfolio

---
+++ v.1/v.1.1/Portfolio/web_design/snippets/inferno_snippet.php
@@ -0,0 +1,15 @@
+<div class="col-md-4 col-sm-6 col-xs-12 portfolio-item webdevelopment">
+    <a class="open-project" href="https://www.infernofitness.ie" target="_blank">
+        <div class="portfolio-column">
+            <div class="portfolio-hover">
+                <div class="portfolio-content">
+                    <span class="title">Inferno Fitness</span>
+                    <hr>
+                    <p>View details.</p>
+                </div>
+                <div class="portfolio-overlay"></div>
+            </div>
+                <img class="lazyload" data-src="/../portfolio/web_design/thumbnails/inferno.png" alt="Inferno Fitness Gym">
+        </div>
+    </a>
+</div>
--- v.1/v.1.1/includes/portfolio.php
+++ v.1/v.1.1/includes/portfolio.php
@@ -14,69 +14,39 @@
                         </ul>
                     </div>
                     <div class="portfolio-container row isotope" id="portfolio-container">
-                        <!-- WORK 1 START -->
-                            <?php include("portfolio/photography/snippets/coronado_snippet.php");?>
-                        <!-- WORK 1 END -->
+                        <?php include("portfolio/photography/snippets/coronado_snippet.php");?>
 
-                        <!-- WORK 2 START -->
-                            <?php include("portfolio/web_design/snippets/bodynamics_snippet.php");?>
-                        <!-- WORK 2 END -->
+                        <?php include("portfolio/web_design/snippets/bodynamics_snippet.php");?>
 
-                        <!-- WORK 3 START -->
-                            <?php include("portfolio/photography/snippets/paris_snippet.php");?>
-                        <!-- WORK 3 END -->
+                        <?php include("portfolio/photography/snippets/paris_snippet.php");?>
 
-                        <!-- WORK 4 START -->
-                          <?php include("portfolio/videography/snippets/summer_2018_snippet.php");?>
-                        <!-- WORK 4 END -->
+                        <?php include("portfolio/videography/snippets/summer_2018_snippet.php");?>
 
-                        <!-- WORK 5 START -->
-                            <?php include("portfolio/photography/snippets/grand_canyon_snippet.php");?>
-                        <!-- WORK 5 END -->
+                        <?php include("portfolio/photography/snippets/grand_canyon_snippet.php");?>
 
-                        <!-- WORK 6 START -->
-                            <?php include("portfolio/videography/snippets/new_york_snippet.php");?>
-                        <!-- WORK 6 END -->
+                        <?php include("portfolio/web_design/snippets/inferno_snippet.php");?>
 
-                        <!-- WORK 7 START -->
-                            <?php include("portfolio/web_design/snippets/casinosource_snippet.php");?>
-                        <!-- WORK 7 END -->
+                        <?php include("portfolio/videography/snippets/new_york_snippet.php");?>
 
-                        <!-- WORK 8 START -->
-                            <?php include("portfolio/photography/snippets/london_snippet.php");?>
-                        <!-- WORK 8 END -->
+                        <?php include("portfolio/web_design/snippets/casinosource_snippet.php");?>
 
-                        <!-- WORK 9 START -->
-                            <?php include("portfolio/web_design/snippets/top_pa_sportsbooks_snippet.php");?>
-                        <!-- WORK 9 END -->
+                        <?php include("portfolio/photography/snippets/london_snippet.php");?>
 
-                        <!-- WORK 10 START -->
-                            <?php include("portfolio/videography/snippets/new_york_snippet.php");?>
-                        <!-- WORK 10 END -->
+                        <?php include("portfolio/web_design/snippets/top_pa_sportsbooks_snippet.php");?>
 
-                        <!-- WORK 11 START -->
-                            <?php include("portfolio/web_design/snippets/casinos_co_nz_snippet.php");?>
-                        <!-- WORK 11 END -->
+                        <?php include("portfolio/videography/snippets/new_york_snippet.php");?>
 
-                        <!-- WORK 12 START -->
-                            <?php include("portfolio/videography/snippets/new_york_snippet.php");?>
-                        <!-- WORK 12 END -->
+                        <?php include("portfolio/web_design/snippets/casinos_co_nz_snippet.php");?>
 
-                        <!-- WORK 13 START -->
-                            <?php include("portfolio/photography/snippets/los_angeles_snippet.php");?>
-                        <!-- WORK 13 END -->
+                        <?php include("portfolio/videography/snippets/new_york_snippet.php");?>
 
-                        <!-- WORK 14 START -->
-                            <?php include("portfolio/videography/snippets/london_snippet.php");?>
-                        <!-- WORK 14 END -->
-                        
-                        <!-- WORK 15 START -->
-                            <?php include("portfolio/photography/snippets/bodynamics_snippet.php");?>
-                        <!-- WORK 15 END -->
+                        <?php include("portfolio/photography/snippets/los_angeles_snippet.php");?>
 
-                        <!-- WORK 16 START -->
-                            <?php include("portfolio/web_design/snippets/hello_caoimhe_snippet.php");?>
-                        <!-- WORK 16 END -->
+                        <?php include("portfolio/videography/snippets/london_snippet.php");?>
+                    
+                        <?php include("portfolio/photography/snippets/bodynamics_snippet.php");?>
+
+                        <?php include("portfolio/web_design/snippets/hello_caoimhe_snippet.php");?>
                     </div>
                 </div>
             </div>
